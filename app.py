0065f00f9bbaa66f5b41f9179b037a8dd2a2618a
Rahul Churiwal <rahulchuriwal7@gmail.com>
2024-12-07 19:35:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pickle
 import requests
+import webbrowser
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     try:
-        response = requests.get(url, timeout=10)  # Set a timeout of 10 seconds
-        response.raise_for_status()  # Raise an exception for HTTP errors
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
         data = response.json()
         poster_path = data.get('poster_path', '')
         if poster_path:
@@ -15,24 +16,30 @@ def fetch_poster(movie_id):
         st.error("The request to TMDB timed out. Please try again later.")
     except requests.exceptions.RequestException as e:
         st.error(f"An error occurred: {e}")
-    return "https://via.placeholder.com/500x750?text=Poster+Not+Available"  # Fallback poster
+    return "https://via.placeholder.com/500x750?text=Poster+Not+Available"
+
+def fetch_wikipedia_url(movie_title):
+    search_url = f"https://en.wikipedia.org/wiki/{movie_title.replace(' ', '_')}"
+    return search_url
 
 def recommend(movie):
     index_value = movies_list[movies_list['title'] == movie].index[0]
-    index=int(index_value)
-    distances = sorted(list(enumerate(similarity[index])),reverse=True,key = lambda x: x[1])[1:6]
-
-    recommended_movies=[]
+    index = int(index_value)
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies = []
     recommended_movie_posters = []
+    recommended_movie_urls = []
     for i in distances:
         movie_id = movies_list.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movies.append(movies_list.iloc[i[0]].title)
-    return recommended_movies,recommended_movie_posters
+        movie_title = movies_list.iloc[i[0]].title
+        recommended_movies.append(movie_title)
+        recommended_movie_urls.append(fetch_wikipedia_url(movie_title))
+    return recommended_movies, recommended_movie_posters, recommended_movie_urls
 
 movies_list = pickle.load(open('movie_list.pkl', 'rb'))
 movie_list = movies_list['title'].values
-similarity=pickle.load(open('similarity.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 st.title("Movie Recommender System")
 
@@ -42,23 +49,20 @@ selected_movie = st.selectbox(
 )
 
 if st.button("Recommend"):
-    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    recommended_movie_names, recommended_movie_posters, recommended_movie_urls = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+    columns = [col1, col2, col3, col4, col5]
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
-
+    for idx, col in enumerate(columns):
+        with col:
+            link = recommended_movie_urls[idx]
+            text = recommended_movie_names[idx]
+            image = recommended_movie_posters[idx]
+            st.markdown(
+                f'<a href="{link}" target="_blank" style="text-decoration:none;" title="Go to Wikipedia"><img src="{image}" alt="{text}" width="100%"></a>',
+                unsafe_allow_html=True
+            )
+            st.markdown(
+                f'<a href="{link}" target="_blank" style="text-decoration:none;" title="Go to Wikipedia">{text}</a>',
+                unsafe_allow_html=True
+            )
